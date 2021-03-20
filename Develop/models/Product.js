aa4845156745e46cce8c9421567236fb1577e629
rnick1 <rnick0260@gmail.com>
2021-03-20 15:48:51 -0700

    Added more information to the Product.js model.

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -19,6 +19,30 @@ Product.init(
       type: DataTypes.STRING,
       allowNull: false
     },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    category_id: {
+      id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'category',
+          key: 'id'
+        }
+      }
+    }
   },
   {
     sequelize,
